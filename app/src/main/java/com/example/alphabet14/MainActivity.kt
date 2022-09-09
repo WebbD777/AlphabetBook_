22aa729cf10c5376bba1c0e595f788f7333298a0
WebbD777 <d.webb99@outlook.com>
2022-09-09 23:40:22 +0200

    Got Internal storage images saved working

--- app/src/main/java/com/example/alphabet14/MainActivity.kt
+++ app/src/main/java/com/example/alphabet14/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.nfc.Tag
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
@@ -13,7 +14,9 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.*
+import java.lang.Exception
 import java.lang.NullPointerException
+import java.lang.reflect.Field
 
 
 //import sun.net.ext.ExtendedSocketOptions.options
@@ -36,32 +39,70 @@ class MainActivity : AppCompatActivity() {
 
         val context:Context = applicationContext
        // imageView.setImageResource(R.drawable.alp)
+        var ii = resources.getIdentifier("slide04", "drawable", packageName)
+        Log.e("DAKE", ii.toString())
 
 
-        save(context)
+        save(context,loopRes())
 
         //switchActivity()
     }
 
-    fun save(context:Context){
-        var bm = BitmapFactory.decodeResource(resources, R.drawable.slide02)
-        //val dirPath = context.filesDir.absolutePath
-        val dirPath ="/storage/emulated/0/DCIM"// Environment.DIRECTORY_DCIM
-        val savPath = File(dirPath, "/alphabet")
-        savPath.mkdir()
+    fun loopRes():IntArray{
 
-        val outFile = File(savPath, "slide7.gif")
+        var resArrID = IntArray(26)
 
+        for (i in 1..26){
+            if (i < 10) {
+                var draw: String = "slide0"+i
+                //var draw: String = "drawable/slide0.gif"
 
-       // var file = File(dirPath, "alphabetbook.png")
-        var outStream = FileOutputStream(outFile)
-        bm.compress(Bitmap.CompressFormat.PNG, 75, outStream)
-        outStream.flush()
-        outStream.close()
+                var id:Int = resources.getIdentifier(draw, "drawable", packageName)
+                if (i==1){Log.e("Sheldon", id.toString())}
+                resArrID[i-1] = id
+            }
+            else if (i >= 10){
+                var draw: String = "slide"+i
+                var id:Int = resources.getIdentifier(draw, "drawable", packageName)
+                resArrID[i-1] = id
+            }
 
+        }
+
+        return resArrID
+       /* var idFields = R.drawable::class.typeParameters
+        var resArr = IntArray(26)
+        for (i in 0..25){
+
+        }*/
+    }
+
+    fun save(context:Context, idList:IntArray){
+
+        var count: Int = 1
+
+        for (i in idList) {
+
+         //   var bm = BitmapFactory.decodeResource(resources, R.drawable.slide03)
+            var bm = BitmapFactory.decodeResource(resources, i)
+            //val dirPath = context.filesDir.absolutePath
+            val dirPath = "/storage/emulated/0/DCIM"// Environment.DIRECTORY_DCIM
+            val savPath = File(dirPath, "/alphabet")
+            savPath.mkdir()
+
+            val outFile = File(savPath, "slide"+count+".gif")
+            count++
+
+            // var file = File(dirPath, "alphabetbook.png")
+            var outStream = FileOutputStream(outFile)
+            bm.compress(Bitmap.CompressFormat.PNG, 75, outStream)
+            outStream.flush()
+            outStream.close()
+
+        }
       //  var biy = load(outFile, savPath.absolutePath.toString())
-        var biy:Bitmap = BitmapFactory.decodeFile(outFile.absolutePath.toString())
-        imageView.setImageBitmap(biy)
+       // var biy:Bitmap = BitmapFactory.decodeFile(outFile.absolutePath.toString())
+       // imageView.setImageBitmap(biy)
 
     }
 
@@ -76,9 +117,19 @@ class MainActivity : AppCompatActivity() {
             var fileInputStream = context.openFileInput(file.absolutePath.toString());
             bitmap = BitmapFactory.decodeStream(fileInputStream);
             fileInputStream.close();
-        } catch(e:FileNotFoundException) {
-          Log.e("MainActivity",e.printStackTrace().toString())
-            Toast.makeText(this@MainActivity, "Null Bitmap", Toast.LENGTH_SHORT).show()
+        } catch(e:Exception ) {
+
+            when(e){
+                is FileNotFoundException ->{
+                    Toast.makeText(this@MainActivity, "Image not found", Toast.LENGTH_SHORT).show()
+                }
+
+                is NullPointerException ->{
+                    Toast.makeText(this@MainActivity, "Null Bitmap", Toast.LENGTH_SHORT).show()
+                }
+            }
+        //  Log.e("MainActivity",e.printStackTrace().toString())
+
 
         }
         return bitmap
